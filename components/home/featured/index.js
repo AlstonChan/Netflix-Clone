a238a3172b07d5c5f12b7a30d0bf1578e005f0ee
Chan Alston <51868695+AlstonChan@users.noreply.github.com>
2022-01-01 17:54:23 +0800

    update featured\index.js

---
+++ components/home/featured/index.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import styles from "../../../styles/featured.module.css";
+
+import Header from "../../header";
+import Input from "./input";
+
+export default function Featured() {
+  const [emailBtnMouseClass, setEmailBtnMouseClass] = useState(
+    `netflixBtn ${styles.getStartedBtn}`
+  );
+
+  function handleMouse(e) {
+    if (e.type === "mousedown") {
+      setEmailBtnMouseClass(
+        `netflixBtn ${styles.getStartedBtn} ${styles.getStartedBtnMouse}`
+      );
+    } else if (e.type === "mouseup" || e.type === "mouseleave") {
+      setEmailBtnMouseClass(`netflixBtn ${styles.getStartedBtn}`);
+    }
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.children[0].firstChild.value);
+  }
+
+  return (
+    <>
+      <section className={styles.headFea}>
+        <div className={styles.shadeFea}>
+          <Header />
+          <div className={styles.headFeaContain}>
+            <h1 className={styles.feaTitle}>
+              Unlimited movies, TV shows, and more.
+            </h1>
+            <p className={styles.feaDesc}>Watch anywhere. Cancel anytime.</p>
+            <p className={styles.feaDescSmall}>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </p>
+            <form
+              action=""
+              autoComplete="on"
+              name="emailInput"
+              onSubmit={(e) => handleFormSubmit(e)}
+              className={styles.flexForm}
+            >
+              <Input />
+              <div className={styles.buttonContain}>
+                <button
+                  type="submit"
+                  className={emailBtnMouseClass}
+                  onMouseDown={(e) => handleMouse(e)}
+                  onMouseUp={(e) => handleMouse(e)}
+                  onMouseLeave={(e) => handleMouse(e)}
+                >
+                  Get Started &nbsp;
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-import Featured from "../components/home/featured";
+import Featured from "../components/home/featured/index";
 
 export default function Home() {
   return (
