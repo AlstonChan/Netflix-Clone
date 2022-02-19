aae716883d482eb4e31a08fe8437baf235fe2a36
Chan Alston <51868695+AlstonChan@users.noreply.github.com>
2022-02-19 16:21:19 +0800

    Update fetchmovie.js
    ...

--- pages/api/fetchmovie.js
+++ pages/api/fetchmovie.js
@@ -47,57 +47,64 @@ const trendingType = ["all", "movie", "tv"];
 
 export default async function handler(req, res) {
   const { requiredKey, requestedData } = req.body;
-  if (requiredKey === process.env.FETCH_KEY && req.method === "POST") {
-    if (requestedData === "hom") {
-      const movies = [];
-      let pageIndex = 1;
-      for (let x = 0; x < movieGenres.length; x++) {
-        if (pageIndex > 3) pageIndex = pageIndex - 1;
-        const url = `/movie?api_key=${process.env.MOVIE_DB_API_KEY}&with_genres=${movieGenres[x].id}&page=${pageIndex}`;
-        pageIndex++;
-        const res = await instances.get(url);
-        const data = res.data;
-        movies.push({ genre: movieGenres[x].name, data });
-      }
-      res.status(200).json({ movies });
-      res.end();
-    } else if (requestedData === "tvs") {
-      const movies = [];
-      let pageIndex = 1;
-      for (let x = 0; x < tvGenres.length; x++) {
-        if (pageIndex > 3) pageIndex = pageIndex - 1;
-        if (tvGenres[x].id == 37) pageIndex = 1;
-        const url = `/tv?api_key=${process.env.MOVIE_DB_API_KEY}&with_genres=${tvGenres[x].id}&page=${pageIndex}`;
-        pageIndex++;
-        const res = await instances.get(url);
-        const data = res.data;
-        movies.push({ genre: tvGenres[x].name, data });
-      }
-      res.status(200).json({ movies });
-      res.end();
-    } else if (requestedData === "new") {
-      const movies = [];
-      let pageIndex = 1;
-      for (let x = 0; x < trendingType.length; x++) {
-        if (trendingType[x] == "all") {
-          pageIndex = 5;
-        } else {
-          pageIndex = 1;
+  try {
+    if (requiredKey === process.env.FETCH_KEY && req.method === "POST") {
+      if (requestedData === "hom") {
+        const movies = [];
+        let pageIndex = 1;
+        for (let x = 0; x < movieGenres.length; x++) {
+          if (pageIndex > 3) pageIndex = pageIndex - 1;
+          const url = `/movie?api_key=${process.env.MOVIE_DB_API_KEY}&with_genres=${movieGenres[x].id}&page=${pageIndex}`;
+          pageIndex++;
+          const res = await instances.get(url);
+          const data = res.data;
+          movies.push({ genre: movieGenres[x].name, data });
         }
-        const url = `https://api.themoviedb.org/3/trending/${trendingType[x]}/week?api_key=${process.env.MOVIE_DB_API_KEY}&page=${pageIndex}`;
-        const res = await instances.get(url);
-        const data = res.data;
-        movies.push({
-          genre:
-            trendingType[x].charAt(0).toUpperCase() + trendingType[x].slice(1),
-          data,
-        });
+        res.status(200).json({ movies });
+        res.end();
+      } else if (requestedData === "tvs") {
+        const movies = [];
+        let pageIndex = 1;
+        for (let x = 0; x < tvGenres.length; x++) {
+          if (pageIndex > 3) pageIndex = pageIndex - 1;
+          if (tvGenres[x].id == 37) pageIndex = 1;
+          const url = `/tv?api_key=${process.env.MOVIE_DB_API_KEY}&with_genres=${tvGenres[x].id}&page=${pageIndex}`;
+          pageIndex++;
+          const res = await instances.get(url);
+          const data = res.data;
+          movies.push({ genre: tvGenres[x].name, data });
+        }
+        res.status(200).json({ movies });
+        res.end();
+      } else if (requestedData === "new") {
+        const movies = [];
+        let pageIndex = 1;
+        for (let x = 0; x < trendingType.length; x++) {
+          if (trendingType[x] == "all") {
+            pageIndex = 5;
+          } else {
+            pageIndex = 1;
+          }
+          const url = `https://api.themoviedb.org/3/trending/${trendingType[x]}/week?api_key=${process.env.MOVIE_DB_API_KEY}&page=${pageIndex}`;
+          const res = await instances.get(url);
+          const data = res.data;
+          movies.push({
+            genre:
+              trendingType[x].charAt(0).toUpperCase() +
+              trendingType[x].slice(1),
+            data,
+          });
+        }
+
+        res.status(200).json({ movies });
+        res.end();
       }
-      res.status(200).json({ movies });
-      res.end();
+    } else {
+      res.status(403);
+      res.end(`Error 403`);
     }
-  } else {
-    res.status(403);
-    res.end(`Error 403`);
+  } catch {
+    res.status(500);
+    res.end(`Error 500`);
   }
 }
